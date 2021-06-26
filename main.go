d1d35da11b7f6db661e834f6151fab0f394cc2f0
rui-watanabe <wataru610806@gmail.com>
2021-06-26 14:17:47 +0900

    set config

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"todo/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.LogFile)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+}
