4b7a33b86b2af97b456083accd8caed5b5916403
rui-watanabe <wataru610806@gmail.com>
2021-06-27 13:23:44 +0900

    set generateHtml func

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,23 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"todo/config"
 )
+
+func generateHtml(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	fmt.Println(files)
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
