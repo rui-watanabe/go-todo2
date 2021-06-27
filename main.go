5eecff43b92271b15e6c0450b76c49767b2c459f
rui-watanabe <wataru610806@gmail.com>
2021-06-27 12:39:50 +0900

    set server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"todo/app/controllers"
 	"todo/app/models"
 )
 
@@ -10,56 +11,7 @@ import (
 // "todo/config"
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.LogFile)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-
-	// log.Println("test")
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test2"
-	// u.Email = "test2@gmail.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
-	// u.CreateUser()
-
-	// u.CreateUser()
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("first todo")
-	// fmt.Println(user)
-
-	// u.Name = "test"
-	// u.Email = "test@gmail.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("third todo")
-
-	// todos, _ := models.GetTodos()
-
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	// user3, _ := models.GetUser(3)
-	// todos, _ := user3.GetTodosByUser()
-
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	t, _ := models.GetTodo(4)
-	// t.Content = "fourth todo"
-	t.DeleteTodo()
+	controllers.StartMainServer()
 }
