40f04295580e26b78a5beef1ce3568457c1c110c
rui-watanabe <wataru610806@gmail.com>
2021-07-03 10:50:07 +0900

    add login auth

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
-	// "todo/app/controllers"
+	"todo/app/controllers"
 	"todo/app/models"
 )
 
@@ -13,19 +12,5 @@ import (
 
 func main() {
 	fmt.Println(models.Db)
-
-	// controllers.StartMainServer()
-	user, _ := models.GetUserByEmail("test@gmail.com")
-	fmt.Println(user)
-
-	session, err := user.CreateSession()
-	if err != nil {
-		log.Println(err)
-	}
-
-	fmt.Println(session)
-
-	valid, err := session.CheckSession()
-	fmt.Println(valid)
-
+	controllers.StartMainServer()
 }
