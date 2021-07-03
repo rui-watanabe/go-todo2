37464d2eb2f395c46ce56bbe97bcf8c59c02e5a6
rui-watanabe <wataru610806@gmail.com>
2021-07-03 14:34:32 +0900

    add udpate todo

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -33,7 +33,7 @@ func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err e
 	return sess, err
 }
 
-var validPath = regexp.MustCompile("^/todos/(edit|delte)/([0-9]+)")
+var validPath = regexp.MustCompile("^/todos/(edit|update)/([0-9]+)")
 
 func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -67,5 +67,6 @@ func StartMainServer() error {
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
 	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
