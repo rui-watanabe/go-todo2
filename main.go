5a7f01598ced574b5efe2ff53a62bf087ad570fb
rui-watanabe <wataru610806@gmail.com>
2021-07-03 10:36:15 +0900

    add session methods

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	// "todo/app/controllers"
 	"todo/app/models"
 )
@@ -16,4 +17,15 @@ func main() {
 	// controllers.StartMainServer()
 	user, _ := models.GetUserByEmail("test@gmail.com")
 	fmt.Println(user)
+
+	session, err := user.CreateSession()
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(session)
+
+	valid, err := session.CheckSession()
+	fmt.Println(valid)
+
 }
